b50bdab318ebbb1f1955af680f2894f2b367dd00
Rahul <rahulbarmanraj@gmail.com>
2026-01-16 16:49:55 +0530

    bug

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -8,8 +8,8 @@ const JWT_EXPIRES_IN = '24h';
 const cookieOptions = {
   expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
   httpOnly: true,
-  secure: false,
-  sameSite: 'lax',
+  secure: true,
+  sameSite: 'none',
 };
 
 const createSendToken = (user, statusCode, res) => {
